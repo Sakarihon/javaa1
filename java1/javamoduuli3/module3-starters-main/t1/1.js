41cc43ad218efe27e2e26754e4bb940f2fa310f0
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2024-11-26 01:19:03 +0200

    mod4

--- java1/javamoduuli3/module3-starters-main/t1/1.js
+++ java1/javamoduuli3/module3-starters-main/t1/1.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// Select the element with id "target"
+
 const targetElement = document.getElementById('target');
 
-// Add the class "my-list" to the element
+
 targetElement.classList.add('my-list');
 
-// Add HTML content (list items) to the target element
+
 targetElement.innerHTML = `
   <li>First item</li>
   <li>Second item</li>
